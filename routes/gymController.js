6ac1d5ed8de07fc55a4f694971d5fd60869d494a
unknown <jordan.aumis@bimbamjob.fr>
2021-09-29 15:06:03 +0200

    conflict merge

--- routes/gymController.js
+++ routes/gymController.js
@@ -74,11 +74,115 @@ app.delete("/delete/:id", (req, res) => {
 		req.params.id,
 		(err, data) => {
 			if (!err)
-				res.send(data);
+				res.send(data)
 			else
-				console.log("ERROR UPDATE ", err);
+				console.log("ERROR UPDATE ", err)
 		}
-	);
-});
+	)
+})
+
+
+// ENDPOINTS
+
+// getById
+app.get('/gym/:id', (req, res) => {
+	if (!ObjectId.isValid(req.params.id))
+		res.sendStatus(400).send("Id is not correct : ", req.params.id)
+
+	GymModel.findById(
+		req.params.id,
+		(err, data) => {
+			if (!err)
+				res.send(data)
+			else
+				console.log("ERREUR ", err)
+		}
+	)
+})
+
+// Gym Average Area
+app.get('/average-area', (req, res) =>{
+	GymModel.aggregate(
+		[
+			{
+				$group:
+					{
+						_id: null,
+						avg: { $avg: "$Surface" }
+					}
+			}
+		],
+		(err, data) => {
+			if (!err)
+				res.send(data)
+			else
+				console.log("ERREUR ", err)
+		}
+ )
+})
+
+// documents Count
+app.get('/count', (req, res) => {
+	GymModel.countDocuments({},
+		(err, data) => {
+			if (!err)
+				res.send(data.toString())
+			else
+				console.log("ERROR UPDATE ", err)
+		}
+	)
+})
+
+// Number Gym by town
+app.get('/count-by-town', (req, res) => {
+	GymModel.aggregate(
+		[
+			{
+				$group:
+					{
+						_id: "$Ville",
+						count: {$sum : 1}
+					}
+			},
+			{ $sort : { count : -1 } }
+		],
+		(err, data) => {
+			if (!err)
+				res.send(data)
+			else
+				console.log("ERREUR ", err)
+		}
+ )
+})
+
+// GymArea gt than
+app.get('/area-gt-than/:area', (req, res)=>{
+	const area = Number(req.params.area)
+	GymModel.find({ Surface: {$gt: Number(req.params.area)}}, 
+		(err, data) => {
+		res.send(data)		
+	})
+})
+
+// GymArea lt than
+app.get('/area-lt-than/:area', (req, res)=>{
+	const area = Number(req.params.area)
+	GymModel.find({ Surface: {$lt: Number(req.params.area)}}, 
+		(err, data) => {
+		res.send(data)		
+	})
+})
+
+// gym given sport and day
+app.get('/:day/:sport', (req, res)=>{
+	let sport = req.params.sport.split("b").join(' b');
+	GymModel.find({ "Seances.Jour": req.params.day.toLowerCase(), "SeancesLibelle": sport }, 
+		(err, dataLowCase) => {
+			GymModel.find({ "Seances.Jour": req.params.day.charAt(0).toUpperCase() + req.params.day.slice(1), "SeancesLibelle": sport }, 
+		(err, dataUpper) => {
+		res.send(dataLowCase.concat(dataUpper))
+		})	
+	})
+})
 
 module.exports = app;
