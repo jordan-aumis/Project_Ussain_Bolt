652c9662729b2770b3624e6dab631fb3f69d8a2c
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-09-30 15:03:59 +0200

    added ussain bolt app

--- client/front-angular/src/app/app-routing.module.ts
+++ client/front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UssainBoltAppComponent } from './ussain-bolt-app/ussain-bolt-app.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', component: UssainBoltAppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/front-angular/src/app/app.module.ts
+++ client/front-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { UssainBoltAppComponent } from './ussain-bolt-app/ussain-bolt-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginPageComponent,
+    UssainBoltAppComponent
   ],
   imports: [
     BrowserModule,
